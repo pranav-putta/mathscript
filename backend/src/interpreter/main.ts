5110c67beee1a42c842f29eca05009920ab6e328
Pranav Putta <pranav.putta22@gmail.com>
2020-09-17 01:24:43 -0400

    simplified functions

--- backend/src/interpreter/main.ts
+++
@@ -1,7 +0,0 @@
-import readlineSync from "readline-sync";
-import { interpretSource } from ".";
-
-while (true) {
-  let input = readlineSync.question(">  ");
-  console.log(interpretSource(input));
-}
